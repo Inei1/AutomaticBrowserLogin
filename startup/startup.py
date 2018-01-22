b7b36ab0d9cafab3ea2b7010daa8f6c6e2a34d4e
Inei1 <abnerd951@gmail.com>
2018-01-21 17:30:28 -0800

    almost done with first usable release

--- startup/startup.py
+++ startup/startup.py
@@ -1,45 +1,60 @@
+import os
 from time import sleep
-
 from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
+import selenium.webdriver.chrome.options
+import selenium.webdriver.firefox.options
+import selenium.webdriver.edge.options
 from Crypto.Cipher import AES
 # noinspection PyUnresolvedReferences
 from secret import private_key
 
-# IE command line arguments: https://msdn.microsoft.com/en-us/library/hh826025(v=vs.85).aspx
+# command line arguments
 # Firefox: https://developer.mozilla.org/en-US/docs/Mozilla/Command_Line_Options
 # Chrome: https://peter.sh/experiments/chromium-command-line-switches/
 # Edge: unknown
 
-# web driver downloads:
-# IE: https://www.microsoft.com/en-us/download/details.aspx?id=44069
-# Firefox: https://github.com/mozilla/geckodriver/releases
-# Chrome: https://sites.google.com/a/chromium.org/chromedriver/downloads
-# Edge: https://developer.microsoft.com/en-us/microsoft-edge/tools/webdriver/
+# web driver downloads: http://www.seleniumhq.org/download/  scroll down
 
 
-"""TODO:
+"""
+TODO:
+documentation
+make user info work
+
+For post-release:
 add functionality for firefox/IE/edge
-allow specifying a different web driver path (windows installer?)
 implement arguments functionality
 clean up code
-documentation
 """
 
 
 class Startup:
     def __init__(self):
         # forward slashes are needed because shlex.split() performed by webbrowser only recognizes / and not \
-        self.chrome_driver_path = "D:/Users/PCUser4/Desktop/School/chromedriver_win32/chromedriver"
-        self.options = Options()
-        self.options.add_experimental_option("detach", True)
-        self.options.add_argument("disable-infobars")
+        appdata_dir = os.getenv("APPDATA")
+        appdata_dir = appdata_dir.replace("\\", "/") + "/"
+        self.chrome_driver_path = appdata_dir + "AutomaticBrowserLogin/WebDrivers/chromedriver_win32/chromedriver.exe"
+        # self.firefox_driver_path = appdata_dir + "AutomaticBrowserLogin/WebDrivers/geckodriver-v0.19.1-win64/" \
+        #                                         "geckodriver.exe"
+        # self.edge_driver_path = appdata_dir + "AutomaticBrowserLogin/WebDrivers/MicrosoftWebDriver"
+        os.environ["webdriver.chrome.driver"] = self.chrome_driver_path
+        # need to have these long chains because Options() is the same name across all drivers
+        self.chrome_options = selenium.webdriver.chrome.options.Options()
+        # self.firefox_options = selenium.webdriver.firefox.options.Options()
+        # self.edge_options = selenium.webdriver.edge.options.Options()
+        self.chrome_options.add_experimental_option("detach", True)
+        self.chrome_options.add_argument("disable-infobars")
         # causes to not work
-        # self.options.add_argument("user-data-dir=C:/Users/PCUser4/AppData/Local/Google/Chrome/User Data")
-        self.chrome_driver = webdriver.Chrome(executable_path=self.chrome_driver_path, chrome_options=self.options)
-        self.firefox_driver = webdriver.Firefox()
-        self.internet_explorer_driver = webdriver.Ie()
-        self.edge_driver = webdriver.Edge()
+        appdata_local_dir = os.getenv("LOCALAPPDATA")
+        appdata_local_dir = appdata_local_dir.replace("\\", "/") + "/"
+        self.chrome_options.add_argument("user-data-dir=" + appdata_local_dir + "Google/Chrome/User Data")
+
+        self.chrome_driver = webdriver.Chrome(executable_path=self.chrome_driver_path,
+                                              chrome_options=self.chrome_options)
+        # self.firefox_driver = webdriver.Firefox(executable_path=self.firefox_driver_path,
+        #                                        firefox_options=self.firefox_options)
+        # self.edge_driver = webdriver.Edge(self.edge_driver_path)  # no options for edge
+        # self.edge_driver.close()
         self.driver = None
         # self.driver.set_page_load_timeout(60)
 
@@ -47,19 +62,20 @@ class Startup:
         if browser == 0:
             self.driver = self.chrome_driver
         elif browser == 1:
-            self.driver = self.firefox_driver
-        elif browser == 2:
-            self.driver = self.internet_explorer_driver
+            pass
+        #    self.driver = self.firefox_driver
         elif browser == 3:
-            self.driver = self.edge_driver
+            pass
+        #    self.driver = self.edge_driver
         print("first:", self.driver.current_window_handle)
         print("url:", str(url)[2:-3])
         self.driver.execute_script("window.open('%s')" % str(url)[2:-3])
         self.switch_to_new_tab()
+        sleep(5)
         print(payload)
         print(str(payload["username"])[2:-3], str(payload["password"])[2:-1])
         user = self.driver.find_element_by_xpath("//input[contains(@name, 'user') or contains(@name, 'email') "
-                                                        "or contains(@name, 'login')]")
+                                                 "or contains(@name, 'login')]")
         user.send_keys(str(payload["username"])[2:-3])
         password = self.driver.find_element_by_xpath("//input[contains(@name, 'pass') or contains(@name, 'pw')]")
         password.send_keys(str(payload["password"])[2:-1])
@@ -84,7 +100,7 @@ class Startup:
             print("line", line, "index", index)
             url = line
             username = user_info.readline()
-            browser = user_info.readline()
+            browser = int(user_info.readline())
             arguments = user_info.readline()
             user_info.readline()  # discard pipe (|)
 
@@ -109,6 +125,10 @@ class Startup:
         # close first tab
         self.driver.switch_to.window(self.driver.window_handles[0])
         self.driver.close()
+        # if self.driver != self.firefox_driver:
+        #    self.firefox_driver.close()
+        # elif self.driver != self.chrome_driver:
+        #    self.chrome_driver.close()
 
 
 def run_autologin():
